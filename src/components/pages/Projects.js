414f21504b985b31eaabc1cb7ca68c7034941465
Imam Hanafi <52751983+imamhanafi1998@users.noreply.github.com>
2023-06-26 09:50:22 +0700

    Update Projects.js

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -54,21 +54,20 @@ const Projects = () => {
             <ProjectCards
               title="Simple Note"
               tags="ReactJS, ExpressJS, MongoDB Atlas, Serverless"
-              desc={`Simple Note is my first MERN stack website. Build using MongoDB Atlas as database, ExpressJS and NodeJS as backend and ReactJS as frontend. This website stores some notes. I got tired for transfering long text from my phone into my PC, and the reverse is just as true. So I've built this website for simplifying that.`}
+              desc={`Simple Note is my first MERN stack website. Built using MongoDB Atlas as database, ExpressJS and NodeJS as backend and ReactJS as frontend. This website stores some notes. I got tired for transfering long text from my phone into my PC, and the reverse is just as true. So I've built this website for simplifying that.`}
               image={Note}
-              link="https://note-fe.vercel.app"
             />
             <ProjectCards
               title="Forum Discussion App"
               tags="MERN, Serverless, PWA, Push Notification"
-              desc={`FD-SIFO is my final project website in order to obtain my S.Kom. title. Build using MongoDB Atlas as database, ExpressJS and NodeJS as backend, ReactJS (can be installed as PWA) as frontend and supports realtime push notification using OneSignal helper. Btw it tooks around 1 year to finish.`}
+              desc={`FD-SIFO is my final project website in order to obtain my S.Kom. title. Built using MongoDB Atlas as database, ExpressJS and NodeJS as backend, ReactJS (can be installed as PWA) as frontend and supports realtime push notification using OneSignal helper. Btw it tooks around 1 year to finish.`}
               image={FD_Sifo}
               link="https://fd-sifo.vercel.app"
             />
             <ProjectCards
               title="Simple Card App"
               tags="ReactJS, ExpressJS, MongoDB Atlas, Serverless, React-Spring"
-              desc={`Simple Card App is customized CRUD MERN app with React-Spring built in my spare time. Build using MongoDB Atlas as database, ExpressJS and NodeJS as backend and ReactJS as frontend. This website stores some cards used for greeting peoples you loved one, or something else. It supports custom background, color, text, highlight and many more.`}
+              desc={`Simple Card App is customized CRUD MERN app with React-Spring built in my spare time. Built using MongoDB Atlas as database, ExpressJS and NodeJS as backend and ReactJS as frontend. This website stores some cards used for greeting peoples you loved one, or something else. It supports custom background, color, text, highlight and many more.`}
               image={CardApp}
               link="https://card-from-imam-hanafi.vercel.app"
             />
