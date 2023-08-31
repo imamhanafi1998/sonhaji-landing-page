03d3efad3f53be48cc2f55aef89be1a73f726ba5
Imam Hanafi <52751983+imamhanafi1998@users.noreply.github.com>
2023-08-31 11:17:00 +0700

    Update Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -37,6 +37,8 @@ import AvatarMasked from "../images/avatarMasked.png";
 import AvatarWMusic from "../images/avatarWMusic.gif";
 import MamentApp from "../images/mament.png";
 import Sigud from "../images/sigud.png";
+import FD_Sifo from "../images/fd-sifo.png"
+import CardApp from "../images/card.png"
 
 import ProjectCards from "../ProjectCards";
 
@@ -426,16 +428,18 @@ const Home = () => {
           </Text>
           <SimpleGrid columns={{ base: "1", md: "2" }} spacing={10}>
             <ProjectCards
-              title="MamentApp"
-              tags="ASP.NET, C#, MVC5, EF6, Razor, SQL Server"
-              desc={`MaMentApp (Matrix Management App) is a Knowledge Management System I've built for my internship program in PT PERTAMINA MOR V. Built using C# in ASP.NET, MVC5 as MVC framework, EF6 for model mapper and Razor View Engine for view.`}
-              image={MamentApp}
+              title="Forum Discussion App"
+              tags="MERN, Serverless, PWA, Push Notification"
+              desc={`FD-SIFO is my final project website in order to obtain my S.Kom. title. Built using MongoDB Atlas as database, ExpressJS and NodeJS as backend, ReactJS (can be installed as PWA) as frontend and supports realtime push notification using OneSignal helper. Btw it tooks around 1 year to finish.`}
+              image={FD_Sifo}
+              link="https://fd-sifo.vercel.app"
             />
             <ProjectCards
-              title="SIGUD"
-              tags="ADMINLTE 3, PHP, jQuery, AJAX, MySQL"
-              desc={`SIGUD (Sistem Informasi Pergudangan) is a Simple Warehouse Management System, built using AdminLTE 3 framework and jQuery for client-side and php native for server-side as demo for fulfilling my college course "Human-Computer Interaction".`}
-              image={Sigud}
+              title="Simple Card App"
+              tags="ReactJS, ExpressJS, MongoDB Atlas, Serverless, React-Spring"
+              desc={`Simple Card App is customized CRUD MERN app with React-Spring built in my spare time. Built using MongoDB Atlas as database, ExpressJS and NodeJS as backend and ReactJS as frontend. This website stores some cards used for greeting peoples you loved one, or something else. It supports custom background, color, text, highlight and many more.`}
+              image={CardApp}
+              link="https://card-from-imamhanafi.vercel.app"
             />
           </SimpleGrid>
           <Flex justifyContent="flex-end" mt="10">
