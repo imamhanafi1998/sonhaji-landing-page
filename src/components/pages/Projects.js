99853f23d13c521b507be7fd10fa2456b4b4e59d
Imam Hanafi <52751983+imamhanafi1998@users.noreply.github.com>
2023-09-04 16:46:23 +0700

    Update Projects.js

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -30,44 +30,44 @@ const Projects = () => {
             My Projects
           </Text>
           <Text fontSize="lg" mb="8">
-            Here are my projects I have done at the past.
+             Here are the projects I have done in the past.
           </Text>
           <SimpleGrid columns={{ base: "1", md: "2" }} spacing={10}>
             <ProjectCards
               title="MamentApp"
               tags="ASP.NET, C#, MVC5, EF6, Razor, SQL Server"
-              desc={`MaMentApp (Matrix Management App) is a Knowledge Management System I've built for my internship program in PT PERTAMINA MOR V. Built using C# in ASP.NET, MVC5 as MVC framework, EF6 for model mapper and Razor View Engine for view.`}
+              desc={`MaMentApp (Matrix Management App) is a knowledge management system I built for my internship program in PT PERTAMINA MOR V. Built using C# in ASP.NET, MVC5 as MVC framework, EF6 for model mapper, and Razor View Engine for view.`}
               image={MamentApp}
             />
             <ProjectCards
               title="SIGUD"
               tags="ADMINLTE 3, PHP, jQuery, AJAX, MySQL"
-              desc={`SIGUD (Sistem Informasi Pergudangan) is a Simple Warehouse Management System, built using AdminLTE 3 framework and jQuery for client-side and php native for server-side as demo for fulfilling my college course "Human-Computer Interaction".`}
+              desc={`SIGUD (Sistem Informasi Pergudangan) is a simple warehouse management system built using AdminLTE 3 framework and jQuery for the client side and php native for the server side as a demo to fulfill my college course "Human-Computer Interaction".`}
               image={Sigud}
             />
             <ProjectCards
               title="Taman Zakat"
               tags="ADMINLTE 2, PHP, jQuery, AJAX, MySQL"
-              desc={`Taman Zakat is Complex Donation and Payroll System that I've built as my first real project between my team and client. Built using php native, AdminLTE 2 framework and so many JavaScript library inside it. My team has 3 members, my friend and I are contributing as Programmer and my lecturer is contributing as Project Manager. But for some reasons, me and my friend decided to resign, so this project is going to be abandoned, maybe....`}
+              desc={`Taman Zakat is a complex donation and payroll system that I've built as my first real project between my team and client. Built using php native, AdminLTE 2 framework and so many JavaScript libraries in it. My team has 3 members, my friend and I are contributing as programmers and my lecturer is contributing as project manager.`}
               image={TamanZakat}
             />
             <ProjectCards
               title="Simple Note"
               tags="ReactJS, ExpressJS, MongoDB Atlas, Serverless"
-              desc={`Simple Note is my first MERN stack website. Built using MongoDB Atlas as database, ExpressJS and NodeJS as backend and ReactJS as frontend. This website stores some notes. I got tired for transfering long text from my phone into my PC, and the reverse is just as true. So I've built this website for simplifying that.`}
+              desc={`Simple Note is my first MERN stack website. Built using MongoDB Atlas as database, ExpressJS and NodeJS as backend, and ReactJS as frontend. This site stores some notes. I got tired of transferring long texts from my phone to my PC and vice versa. So I've built this website to simplify that.`}
               image={Note}
             />
             <ProjectCards
               title="Forum Discussion App"
               tags="MERN, Serverless, PWA, Push Notification"
-              desc={`FD-SIFO is my final project website in order to obtain my S.Kom. title. Built using MongoDB Atlas as database, ExpressJS and NodeJS as backend, ReactJS (can be installed as PWA) as frontend and supports realtime push notification using OneSignal helper. Btw it tooks around 1 year to finish.`}
+              desc={`FD-SIFO is my final project website in order to obtain my S.Kom. title. Built using MongoDB Atlas as database, ExpressJS and NodeJS as backend, ReactJS (can be installed as PWA) as frontend and supports real-time push notification using OneSignal helper. It took around 1 year to finish.`}
               image={FD_Sifo}
               link="https://fd-sifo.vercel.app"
             />
             <ProjectCards
               title="Simple Card App"
               tags="ReactJS, ExpressJS, MongoDB Atlas, Serverless, React-Spring"
-              desc={`Simple Card App is customized CRUD MERN app with React-Spring built in my spare time. Built using MongoDB Atlas as database, ExpressJS and NodeJS as backend and ReactJS as frontend. This website stores some cards used for greeting peoples you loved one, or something else. It supports custom background, color, text, highlight and many more.`}
+              desc={`Simple Card App is a custom CRUD MERN app built with React-Spring in my spare time. Built using MongoDB Atlas as database, ExpressJS and NodeJS as backend, and ReactJS as frontend. This website stores some cards used for greeting people, your loved ones, or something else. It supports custom background, color, text, highlight and many more.`}
               image={CardApp}
               link="https://card-from-imamhanafi.vercel.app"
             />
